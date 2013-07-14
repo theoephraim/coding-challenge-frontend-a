35b8d5d11f1cd9ee2d9d8aee2849899c49e104e0
Theo Ephraim <theo@highscorehouse.com>
2013-07-14 00:53:59 -0400

    Integrated Typeahead autocomplete

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,22 @@
 try{ Typekit.load(); } catch(e){}
+
+$(document).ready(function(){
+	
+	$('input.city-select').typeahead([{
+		remote: {
+			url: 'http://www.busbud.com/en/complete/locations/%QUERY',
+			dataType: 'jsonp',
+			filter: filterCityData
+		}
+	  }
+	]);
+	
+});
+
+function filterCityData( cities ){
+	for (var i=0; i<cities.length; i++){
+		cities[i].value = cities[i].label;
+		cities[i].tokens = cities[i].norm.split(',');
+	}
+	return cities;
+}
